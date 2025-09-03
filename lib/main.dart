e2b90c0afcfde9563855996c1c3b4b67a55e08fe
ardavan8102 <ardavaneskandari007@gmail.com>
2025-09-04 02:56:24 +0330

    First Commit

---
+++ lib/main.dart
@@ -0,0 +1,26 @@
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'package:todo_application/provider/TodoProvider.dart';
+import 'package:todo_application/screens/homepage_screen.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+  
+  @override
+  Widget build(BuildContext context) {
+    return ChangeNotifierProvider(
+      create: (_) => TodoNotifier(),
+      child: MaterialApp(
+        title: "Todo List App",
+        theme: ThemeData(
+          colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurpleAccent)
+        ),
+        home: const HomepageScreen(),
+      ),
+    );
+  }
+}
---
+++ lib/screens/homepage_screen.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+class HomepageScreen extends StatelessWidget {
+  const HomepageScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: Text('Hello World !'),
+      ),
+    );
+  }
+}
